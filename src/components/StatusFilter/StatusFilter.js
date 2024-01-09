123c5dc603f14b1b30d7fd3667db5952532f9c79
RuslanKorni <ruslankornijcenko4@gmail.com>
2024-01-09 20:57:28 +0200

    first-commit

---
+++ src/components/StatusFilter/StatusFilter.js
@@ -0,0 +1,17 @@
+import { Button } from "components/Button/Button";
+import css from "./StatusFilter.module.css";
+import { useSelector } from "react-redux";
+import { statusFilters } from "../../redux/constants";
+import { getStatusFilter } from "../../redux/selectors";
+
+export const StatusFilter = () => {
+  // Отримуємо значення фільтра із стану Redux
+  const filter = useSelector(getStatusFilter);
+  return (
+    <div className={css.wrapper}>
+      <Button selected={filter === statusFilters.all}>All</Button>
+      <Button selected={filter === statusFilters.active}>Active</Button>
+      <Button selected={filter === statusFilters.completed}>Completed</Button>
+    </div>
+  );
+};
---
+++ src/redux/store.js
@@ -0,0 +1,26 @@
+import { createStore } from 'redux';
+import { devToolsEnhancer } from "@redux-devtools/extension";
+
+//! Початкове значення стану Redux для кореневого редюсера, якщо не передати параметр preloadedState.
+
+const initalState = {
+  tasks: [
+    { id: 0, text: 'Learn HTML and CSS', completed: true },
+    { id: 1, text: 'Get good at JavaScript', completed: true },
+    { id: 2, text: 'Master React', completed: false },
+    { id: 3, text: 'Discover Redux', completed: false },
+    { id: 4, text: 'Build amazing apps', completed: false },
+  ],
+  filters: {
+    status: 'all',
+  },
+};
+
+//! Поки що використовуємо редюсер який тільки повертає отриманий стан
+
+const rootReducer = (state = initalState, action) => {
+    return state;
+}
+const enhancer = devToolsEnhancer();
+export const store = createStore(rootReducer, enhancer)
+ 
